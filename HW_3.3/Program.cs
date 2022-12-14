edc310fc80c9bf16a5a033a9d540b628e036efac
Arsen <hoabs@mail.ru>
2022-12-14 14:46:01 +0300

    Added HW_3.3

---
+++ HW_3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter an integer number");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = "";
+
+if (n < 0)
+{
+    n = -n;
+}
+
+for (int i = 1; i < n; i++)
+{
+result = result+$"{Math.Pow(i,3)}, ";
+}
+
+result = result+$"{Math.Pow(n,3)}";
+
+Console.WriteLine(result);
