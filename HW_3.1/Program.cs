b9fbc1282579c3aab3cc164675b3a0ffeeb2c939
Arsen <hoabs@mail.ru>
2022-12-14 14:13:21 +0300

    Initial Commit + Added HW_3.1

---
+++ HW_3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a positive 5-digit number");
+string? a = Console.ReadLine();
+while (a.Length != 5)
+{
+Console.WriteLine("Error. Enter a positive 5-digit number");
+a = Console.ReadLine();
+}
+if (a[0]==a[4] && a[1]==a[3])
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}
